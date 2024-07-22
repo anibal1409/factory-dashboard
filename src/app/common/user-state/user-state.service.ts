fba272bd23f86ce229357d04c77d0c09bf61a4f8
anibal1409 <anibal.antonio1409@gmail.com>
2024-07-22 03:11:41 -0400

    fix: ajustar select de reporte de ventas

--- src/app/common/user-state/user-state.service.ts
+++ src/app/common/user-state/user-state.service.ts
@@ -22,17 +22,17 @@ export class UserStateService {
 
   setUser(user: UserStateVM | null): void {
     this.user$.next(user);
-    localStorage.setItem('miprofe-user', JSON.stringify(user));
+    localStorage.setItem('danko-user', JSON.stringify(user));
   }
 
   clear(): void {
     this.user$.next(null);
-    localStorage.removeItem('miprofe-user');
+    localStorage.removeItem('danko-user');
   }
 
   getUserStorage(): UserStateVM | null {
     let user = null;
-    const userString = localStorage.getItem('miprofe-user');
+    const userString = localStorage.getItem('danko-user');
     if (userString) {
       user = JSON.parse(userString) as UserStateVM;
     }
